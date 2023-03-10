1557be8a912775a4d5059b4832ff11210e93af85
zhaoyuhang <1033106103@qq.com>
2023-03-10 13:55:29 +0800

    fix 消息队列初始化

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const amqp = require('amqp-connection-manager');
-const streamPlugin = require('amqp-0-9-1-stream');
-
+const amqp = require("amqp-connection-manager");
 // 创建连接管理器
-const connection = amqp.connect(['amqp://localhost']);
+const connection = amqp.connect({
+  hostname: "192.168.5.9",
+  port: 5672,
+  username: "wisdom",
+  password: "wisdom0826",
+});
+// 创建通道
 const channelWrapper = connection.createChannel({
   json: true,
-  setup: function(channel) {
-    // 安装流插件
-    return channel.useStreamPublishConfirm(streamPlugin);
-  }
 });
 
-// 发送数据
-const data = 'hello, world';
-channelWrapper.publish('my-exchange', 'my-routing-key', data, {
-  // 设置持久化选项
-  persistent: true,
-  // 设置流选项
-  mandatory: true,
-  confirm: function(err, msg) {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Message published: ' + JSON.stringify(msg));
-    }
+// 定义队列名称
+const queueName = "test_queue";
+
+// 创建队列并发送消息
+async function createQueueAndSendMessage() {
+  // 声明队列
+  await channelWrapper.addSetup((channel) => {
+    return channel.assertQueue(queueName, {
+      durable: true,
+    });
+  });
+
+  // 发送消息
+  for (let index = 0; index < 10; index++) {
+    const element = index;
+    channelWrapper.sendToQueue(queueName, { element });
   }
-});
+}
+
+// 发送消息
+createQueueAndSendMessage()
+  .then(() => {
+    console.log("Message sent successfully!");
+  })
+  .catch((err) => {
+    console.error("Error sending message:", err);
+  });
--- index2.js
+++ index2.js
@@ -1,21 +1,46 @@
-const amqp = require('amqp-connection-manager');
-const streamPlugin = require('amqp-0-9-1-stream');
-
+const amqp = require("amqp-connection-manager");
 // 创建连接管理器
-const connection = amqp.connect(['amqp://localhost']);
+const connection = amqp.connect({
+  hostname: "192.168.5.9",
+  port: 5672,
+  username: "wisdom",
+  password: "wisdom0826",
+});
+// 创建通道
 const channelWrapper = connection.createChannel({
   json: true,
-  setup: function(channel) {
-    // 安装流插件
-    return channel.useStreamConsumer(streamPlugin);
-  }
 });
 
-// 接收数据
-channelWrapper.addSetup(function(channel) {
-  // 订阅队列
-  return channel.consume('my-queue', function(msg) {
-    console.log('Message received: ' + msg.content.toString());
-    channel.ack(msg);
+// 定义队列名称
+const queueName = "test_queue";
+
+// 处理队列中的消息
+function handleMessages(message) {
+  console.log("Received message:", message.content.toString());
+  channelWrapper.ack(message);
+}
+
+// 订阅队列并处理消息
+async function subscribeToQueue() {
+  // 声明队列
+  await channelWrapper.addSetup((channel) => {
+    channel.prefetch(1)
+    return channel.assertQueue(queueName, {
+      durable: true
+    });
   });
-});
+
+  // 订阅队列并处理消息
+  await channelWrapper.addSetup((channel) => {
+    return channel.consume(queueName, handleMessages);
+  });
+}
+
+// 订阅队列并处理消息
+subscribeToQueue()
+  .then(() => {
+    console.log("Subscribed to queue successfully!");
+  })
+  .catch((err) => {
+    console.error("Error subscribing to queue:", err);
+  });
